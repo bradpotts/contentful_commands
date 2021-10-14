ee3fcdd5bc44f7209c05cae702eca0f4a59f6ea7
Brad Potts <brad.potts@phcnet.co>
2021-10-14 04:19:17 +0000

    initial upload.

---
+++ src/components/Field.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Button,
+    EditorToolbarButton,
+    Table,
+    TableBody,
+    TableRow,
+    TableCell,
+    TextField,
+} from '@contentful/forma-36-react-components';
+import tokens from '@contentful/forma-36-tokens';
+import { FieldExtensionSDK } from '@contentful/app-sdk';
+import { v4 as uuid } from 'uuid';
+
+interface FieldProps {
+    sdk: FieldExtensionSDK;
+}
+
+/* Tutorial Repeater Items */
+interface Item {
+    id: string;
+    command_item_title: string;
+    command_item_description: string;
+    command_item_copy_command: string;
+}
+
+/** A simple utility function to create a 'blank' item
+ * @returns A blank `Item` with a uuid
+*/
+function createItem(): Item {
+    return {
+        id: uuid(),
+        command_item_title: '',
+        command_item_description: '',
+        command_item_copy_command: '',
+    };
+}
+
+/** The Field component is the Repeater App which shows up 
+ * in the Contentful field.
+ * 
+ * The Field expects and uses a `Contentful JSON field`
+ */
+const Field = (props: FieldProps) => {
+    const { valueName = 'Value' } = props.sdk.parameters.instance as any;
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        // This ensures our app has enough space to render
+        props.sdk.window.startAutoResizer();
+
+        // Every time we change the value on the field, we update internal state
+        props.sdk.field.onValueChanged((value: Item[]) => {
+            if (Array.isArray(value)) {
+                setItems(value);
+            }
+        });
+    });
+
+    /** Adds another item to the list */
+    const addNewItem = () => {
+        props.sdk.field.setValue([...items, createItem()]);
+    };
+
+    /** Creates an `onChange` handler for an item based on its `property`
+     * @returns A function which takes an `onChange` event 
+    */
+    const createOnChangeHandler = (item: Item, property: 'command_item_title' | 'command_item_description' | 'command_item_copy_command') => (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const itemList = items.concat();
+        const index = itemList.findIndex((i) => i.id === item.id);
+
+        itemList.splice(index, 1, { ...item, [property]: e.target.value });
+
+        props.sdk.field.setValue(itemList);
+    };
+
+    /** Deletes an item from the list */
+    const deleteItem = (item: Item) => {
+        props.sdk.field.setValue(items.filter((i) => i.id !== item.id));
+    };
+
+    return (
+        <div>
+            <Table>
+                <TableBody>
+                    {items.map((item) => (
+                        <TableRow key={item.id}>
+                            <TableCell>
+                                <TextField
+                                    id="command_item_title"
+                                    name="command_item_title"
+                                    labelText="Command Title"
+                                    value={item.command_item_title}
+                                    onChange={createOnChangeHandler(item, 'command_item_title')}
+                                />
+                            </TableCell>
+                            <TableCell>
+                                <TextField
+                                    id="command_item_description"
+                                    name="command_item_description"
+                                    labelText="Command Description"
+                                    value={item.command_item_description}
+                                    onChange={createOnChangeHandler(item, 'command_item_description')}
+                                />
+                            </TableCell>
+                            <TableCell>
+                                <TextField
+                                    id="command_item_copy_command"
+                                    name="command_item_copy_command"
+                                    labelText="Command Copy CLI"
+                                    value={item.command_item_copy_command}
+                                    onChange={createOnChangeHandler(item, 'command_item_copy_command')}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <EditorToolbarButton
+                                    label="delete"
+                                    icon="Delete"
+                                    onClick={() => deleteItem(item)}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <Button
+                buttonType="naked"
+                onClick={addNewItem}
+                icon="PlusCircle"
+                style={{ marginTop: tokens.spacingS }}
+            >
+                Add Item
+            </Button>
+        </div>
+    );
+};
+
+export default Field;
